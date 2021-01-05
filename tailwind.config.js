21057c59c82966f80dd1522e8c33ebdbcb1c31cf
Maciej Czarkowski <maciej.czarkowski@emergeconsulting.pl>
2021-01-05 18:40:49 +0100

    add tailwind configuration and other assets

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        './resources/js/**/*.vue',
+        './resources/views/**/*.php',
+    ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -13,5 +12,14 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('tailwindcss'),
     ]);
+    // .webpackConfig({
+    //     plugins: [
+    //         new WebpackShellPlugin({
+    //             onBuildStart: {
+    //                 scripts: ['php artisan config:js']
+    //             },
+    //         }),
+    //     ],
+    // });
