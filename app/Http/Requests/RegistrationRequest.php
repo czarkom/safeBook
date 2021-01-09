b56aa93b850876b152a751b97e0037e4789bb3cf
Maciej Czarkowski <maciej.czarkowski@emergeconsulting.pl>
2021-01-09 13:21:45 +0100

    add registration form validation regex

--- app/Http/Requests/RegistrationRequest.php
+++ app/Http/Requests/RegistrationRequest.php
@@ -26,6 +26,7 @@ class RegistrationRequest extends FormRequest
             'email.email' => 'Nieprawidłowy format adresu email',
             'password.required' => 'Podaj hasło',
             'password.min' => 'Hasło musi mieć co najmniej 8 znaków',
+            'password.regex' => 'Hasło musi zawierać co najmniej 1 dużą, 1 małą literę, 1 cyfrę oraz 1 znak specjalny (!$#%@&^)',
             'password_repeat.required' => 'Podaj hasło ponownie',
             'password_repeat.same' => 'Powtórzone hasło nieprawidłowe',
         ];
@@ -42,7 +43,7 @@ class RegistrationRequest extends FormRequest
             'first_name' => 'required',
             'last_name' => 'required',
             'email' => 'required|unique:users,email|email',
-            'password' => 'required|min:8',
+            'password' => 'required|min:8|regex:/^.*(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!$#%@&^]).*$/',
             'password_repeat' => 'required|same:password',
         ];
     }
